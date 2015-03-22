9d3a91e304f542c2fe61846fa45d9c8c5a99fece
rotkreis <rotkreis@gmail.com>
2015-03-22 17:21:45 +0800

    All Fin

---
+++ Tridiagonal.h
@@ -0,0 +1,16 @@
+//
+//  Tridiagonal.h
+//  ConvectionDiffusion_1D
+//
+//  Created by Li Xinrui on 11/1/14.
+//  Copyright (c) 2014 ___Imaginaire___. All rights reserved.
+//
+
+#ifndef ConvectionDiffusion_1D_Tridiagonal_h
+#define ConvectionDiffusion_1D_Tridiagonal_h
+#include "Matrix.h"
+
+// Notation & Reference: Wikipedia - Tridiagonal Matrix
+void TridiagonalSolve(mVector& a, mVector& b, mVector& c, mVector& d, mVector& sol);
+
+#endif
